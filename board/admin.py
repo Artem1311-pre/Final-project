2249a74613e4b15f1004485a264b224ffcb3606c
Artem1311-pre <akochkin@toyotatt.ru>
2024-01-11 13:55:30 +0300

    финальный проект

---
+++ board/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post, CustomUser, Reply, Category
+
+admin.site.register(Post)
+admin.site.register(CustomUser)  # Change "Author" to "CustomUser"
+admin.site.register(Reply)
+admin.site.register(Category)
+
+
